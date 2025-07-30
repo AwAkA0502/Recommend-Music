50dbb067f3189c130588eafd5192d3d484d6694e
Rizky <rizkyfebrian.hidayat@gmail.com>
2025-07-31 04:27:27 +0700

    update login

--- recommend-music/src/app/login/page.tsx
+++ recommend-music/src/app/login/page.tsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
     setError(null);
 
     try {
-      const res = await fetch("http://localhost:5050/api/auth/login", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         credentials: "include",
