87e31fbb908d0e8e8d7304aab0108f32b568751e
Rizky <rizkyfebrian.hidayat@gmail.com>
2025-08-11 15:10:23 +0700

    update middleware di python

--- python/script/embedding_user_input_rerank.py
+++ python/script/embedding_user_input_rerank.py
@@ -11,7 +11,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://awakatune.com", "https://www.awakatune.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
